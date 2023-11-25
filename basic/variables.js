e7cce055ad29f285df0cc7df11f1eb99bcffef58
TyagiAnkit1697 <ankittyagi1697@gmail.com>
2023-11-25 14:57:57 +0000

    some basic and devcontainer file

---
+++ basic/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123;
+let accountEmail = "ankt@gmail.com";
+var accountPwd = "12345";
+accountCity = "ut"
+
+//accountId = 234;     //cannot re-assign values in const variables.
+accountEmail = "annkit@gmail.com";
+accountPwd = "4321";
+accountCity = "UP";
+
+console.table([accountId, accountEmail, accountPwd,accountCity]);
